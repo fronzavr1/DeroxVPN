fb4bd9260e4b1dd2913d2128356c811fa10efafe
fronzavr1 <nemikk02@gmail.com>
2026-08-02 16:22:20 +0300

    Update __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,5 @@
+from .start import router as start_router
+from .tariff import router as tariff_router
+from .sub_info import router as sub_info_router
+from .expiration_check import router as expiration_check_router
+from .stars_payment import router as stars_payment_router
